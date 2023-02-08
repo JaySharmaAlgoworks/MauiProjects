1ef14a406772b92980dc8c90d828515ae2fee029
jaysharma <jay.sharma@algoworks.com>
2023-02-08 17:40:53 +0530

    changes

---
+++ MyFirstMauiApp/MyFirstMauiApp/LayoutExample.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MyFirstMauiApp;
+
+public partial class LayoutExample : ContentPage
+{
+	public LayoutExample()
+	{
+		InitializeComponent();
+		VStackLayout.Padding = DeviceInfo.Platform == DevicePlatform.iOS ? new Thickness(10, 20, 10, 20) : new Thickness(20);
+
+    }
+}
